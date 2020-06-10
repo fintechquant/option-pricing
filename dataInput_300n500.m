a8265254fcfe8e709728dd87bf0d70a2585c0576
fintechquant <62858741+fintechquant@users.noreply.github.com>
2020-06-11 01:39:10 +0800

    Add files via upload

---
+++ dataInput_300n500.m
@@ -0,0 +1,59 @@
+%   CSI500
+    load([dirData '\wIdx.mat']);            % wIdx.CSI300
+    load([dirData '\wPrc_Stocks.mat']);
+    load([dirData '\wPrc_Indices.mat']);
+    load([dirData '\wPrc_Events.mat']);
+    load([dirData '\CDSys_Var1.mat']);
+    
+    load([dirData '\wConsensusFY0123.mat']);
+    load([dirData '\factors_lqy\SUE.mat']); 
+    load([dirData '\factors_lqy\RROE.mat']); 
+    load([dirData '\factors_lqy\DROE.mat']); 
+    load([dirData '\factors_lqy\RE2P.mat']);
+    load([dirData '\factors_lqy\DE2P.mat']); 
+    load([dirData '\factors_lqy\turnover3d.mat']); 
+    load([dirData '\factors_lqy\seasonality2.mat']);
+    
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Growth_ROAG.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum1_SUE.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum1_SURI.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum1_SUOI.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum2_BIAS.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Quality_QDROA.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL5D.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL3M.mat']);  
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_BETA.mat']); 
+
+%   CSI300
+    load([dirData '\wPrc_Stocks.mat']);
+    load([dirData '\wPrc_Indices.mat']);
+    load([dirData '\wPrc_Events.mat']);
+    load([dirData '\CDSys_Var1.mat']);
+
+    load([dirData '\wConsensusFY0123.mat']);
+    load([dirData '\factors_lqy\SUE.mat']);
+    load([dirData '\factors_lqy\RROE.mat']); 
+    load([dirData '\factors_lqy\DROE.mat']); 
+    load([dirData '\factors_lqy\RE2P.mat']);
+    load([dirData '\factors_lqy\DE2P.mat']); 
+    load([dirData '\factors_lqy\turnover3d.mat']); 
+    load([dirData '\factors_lqy\seasonality2.mat']);
+
+    load([dirData '\factors_zyd\normalizeAll_winsorize_AdT2_Size_Size.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Value1_DE2P.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Value2_RE2P.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum1_SUE.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Momentum1_SURI.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL.mat']);
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL5D.mat']); 
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_VOL3M.mat']);  
+    load([dirData '\factors_zyd\orthogonalize_normalizeAll_winsorize_AdT2_Sentiment_FFRsqrA1.mat']);
+    
+    
+    
+    
+    
+    
+    
+    
