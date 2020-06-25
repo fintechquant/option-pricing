6fe0a55ce4c458963961c66f45231fec7db61b9b
fintechquant <62858741+fintechquant@users.noreply.github.com>
2020-06-25 10:01:49 +0800

    Delete num2str2.m

--- num2str2.m
+++
@@ -1,7 +0,0 @@
-function result = num2str2(x, FORMAT)
-    result = num2str(x, FORMAT);
-    if  size(x,1)==1
-        ndig   = str2num(strtok(FORMAT,{'%','.','f'}));
-        result = [repmat(' ',1, ndig*size(x,2)-length(result)) result];
-    end
-end
